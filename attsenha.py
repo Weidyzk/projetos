eb81c97e89ace4a631f3c53a4368275ec9877336
Weidyzk <weidylucena@gmail.com>
2025-08-25 16:42:27 -0300

    Add files via upload

---
+++ attsenha.py
@@ -0,0 +1,6 @@
+senha_certa = "python123"
+senha = (input("Digite sua senha: "))
+
+while senha != senha_certa: 
+    senha = (input("Senha incorreta, digite novamente. "))
+print ("Senha está correta, parabêns por descobrir sua senha!")

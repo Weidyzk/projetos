294a835873615006a5edfdd500bf35bc8e7314df
Weidyzk <weidylucena@gmail.com>
2025-08-27 16:26:46 -0300

    Add files via upload

---
+++ attmari2.py
@@ -0,0 +1,7 @@
+A = int(input('Número 1: '))
+B = int(input('Número 2: '))
+dif= A 
+A = B
+B = dif
+
+print (f'Essa é a diferença {A} e {B}')
---
+++ attmari3.py
@@ -0,0 +1,8 @@
+N = int(input('Digite os lados: '))
+for i in range(N):
+    lado = float(input(f'Digite um comprimento do quadrilatero {i+1}: '))
+    area = lado * lado 
+    print(f'Esta é a area do quadrilatero {i+1}: {area}')
+
+
+

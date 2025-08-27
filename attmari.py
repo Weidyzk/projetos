ac50e07e35985b72a11b4eae04f6574d28d48f19
Weidyzk <weidylucena@gmail.com>
2025-08-27 15:19:11 -0300

    Add files via upload

---
+++ attmari.py
@@ -0,0 +1,9 @@
+A = int(input("Digite um número 1: "))
+B = int(input("Digite um número 2: "))
+if A > B:
+    print ('Esse é o número maior.')
+
+elif A < B:
+    print ('Não é um número maior.')
+
+else: print ('errou, bobo')
